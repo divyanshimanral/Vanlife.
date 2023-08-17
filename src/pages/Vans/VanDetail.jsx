3f90de10bd4e6c99f92669159926db59cad30570
Prashansha <manraldivyanshi46@gmail.com>
2023-08-18 00:09:57 +0530

    Project done

---
+++ src/pages/Vans/VanDetail.jsx
@@ -0,0 +1,49 @@
+import React, { Suspense } from "react";
+import {
+  Link,
+  useParams,
+  useLocation,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
+import { getVans } from "../../../api";
+
+export function loader({ params }) {
+  return defer({ van: getVans(params.id) });
+}
+
+export default function VanDetail() {
+  const location = useLocation();
+  const dataPromise = useLoaderData();
+  // console.log(dataPromise)
+
+  const search = location.state?.search || "";
+  const type = location.state?.type || "all";
+
+  function renderVans(van) {
+    return (
+      <div className="van-detail">
+        <img src={van.imageUrl} />
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        <h2>{van.name}</h2>
+        <p className="van-price">
+          <span>${van.price}</span>/day
+        </p>
+        <p>{van.description}</p>
+        <button className="link-button">Rent this van</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="van-detail-container">
+      <Link to={`..${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} vans</span>
+      </Link>
+      <Suspense fallback={<h2>Loading vans.....</h2>}>
+        <Await resolve={dataPromise.van}>{renderVans}</Await>
+      </Suspense>
+    </div>
+  );
+}
